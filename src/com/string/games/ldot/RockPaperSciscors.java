c0f73249559305dd384b86c6b8690caf096256f2
Ldotus <69371448+Ldotus@users.noreply.github.com>
2021-09-09 00:42:03 +0100

    Minor bug fix
    ***Removed***
    option to input score to acquire score and details 
    
    ***Implemented***
    score is printed at the end of every round
    print statement to say if you're winning or losing. 
    
    ***fixed ***
    increased opp score when score was printed

--- src/com/string/games/ldot/RockPaperSciscors.java
+++ src/com/string/games/ldot/RockPaperSciscors.java
@@ -11,7 +11,7 @@ public class RockPaperSciscors {
 	static String oppHand;
 	static int playerScore;
 	static int oppScore;
-	
+
 	static boolean playing = true;
 
 	enum hands {
@@ -40,11 +40,31 @@ public class RockPaperSciscors {
 
 	}
 
+	void printScore() {
+		if (playerScore > oppScore) {
+
+			System.out.println("Score of games so far");
+			System.out.println(playerScore + ":" + oppScore);
+			System.out.println("You're in the lead");
+
+		} else if (playerScore < oppScore) {
+			System.out.println("Score of games so far");
+			System.out.println(playerScore + ":" + oppScore);
+			System.out.println("You're Losing");
+
+		} else {
+			System.out.println(playerScore + ":" + oppScore);
+			System.out.println("you're Drawing!");
+
+		}
+	}
+
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		RockPaperSciscors rockPaperSciscors = new RockPaperSciscors();
 
+		// while loop beaks if user types quit
 		while (true) {
 			System.out.println("Welcome to Rock Paper Sciscors, type a choice");
 
@@ -52,62 +72,46 @@ public class RockPaperSciscors {
 
 			// TODO: attempt to create method that returns random enum
 
-			// checks to see if the string entered is valid as a move against enum
-			// attempted to do with an enum but currently can't achieve returning a random
-			// enum
-
 			if (choice.equals("quit")) {
 				break;
 			}
-			
-			if (choice.equals("score")) {
-				if (playerScore > oppScore) {
-					
-					System.out.println("Score of games so far");
-					System.out.println(playerScore + ":" + oppScore);
-					System.out.println("You're in the lead");
-					
-				}
-				else if (!(playerScore > oppScore)) {
-					System.out.println("Score of games so far");
-					System.out.println(playerScore + ":" + oppScore);
-					System.out.println("You're Losing");
-				
-				}
-				else {
-					System.out.println("you're Drawing!");
-				
-					
-				}	
-				
-			}
-			//move is invalid if inputted string is not rock paper or sciscors 
+
+			// checks to see if the string entered is valid as a move against enum
+			// attempted to do with an enum but currently can't achieve returning a random
+			// enum
+			// move is invalid if inputted string is not rock paper or sciscors
 			if (!choice.equals(hands.rock.toString()) && !choice.equals(hands.paper.toString())
-					&& !choice.equals(hands.sciscors.toString()) && !choice.equals("score")) {
+					&& !choice.equals(hands.sciscors.toString())) {
 
 				System.out.println("invalid move");
 
 			} else {
-
+				// if move is valid sets oppHand to opponentHand
+				// method which returns a string
 				oppHand = rockPaperSciscors.opponentHand();
 
 			}
+			//if both oppHand and user input is the same 
+			
 			if (choice.equals(oppHand)) {
 				System.out.println("You Tied");
-			}
-			if (choice.equals("paper") && oppHand.equals("rock") 
+				
+				//else if statement defining winning parameters
+			} else if (choice.equals("paper") && oppHand.equals("rock")
 					|| choice.equals("rock") && oppHand.equals("sciscors")
 					|| choice.equals("sciscors") && oppHand.equals("paper")) {
 
 				result = "You Win";
 				playerScore++;
 				System.out.println(result);
+				rockPaperSciscors.printScore();
 			}
 
 			else {
 				result = "Loser";
 				oppScore++;
 				System.out.println(result);
+				rockPaperSciscors.printScore();
 			}
 
 		}
